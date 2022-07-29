3795265a6571375ea72f5667d6d03802b4d39441
Jakob Löw <jakob@m4gnus.de>
2022-07-29 18:55:40 +0200

    :sparkles: :art: make the detect script a module which can be used by other programs

--- detect.py
+++ detect.py
@@ -4,76 +4,80 @@ from dt_apriltags import Detector
 
 APRILTAG_AREA_WIDTH = 2710
 APRILTAG_AREA_HEIGHT = 1360
+TABLE_OFFSET_X = 40
+TABLE_OFFSET_Y = 680
 TABLE_WIDTH = 2000
 TABLE_HEIGHT = 1240
+BLACKWHITE_THRESH = 127
+BOX_FILL_THRESH = 240
 
-img = cv2.imread("test-data-real.JPG")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+def analyze_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, img = cv2.threshold(img, BLACKWHITE_THRESH, 255, cv2.THRESH_BINARY)
 
-at_detector = Detector(families='tag36h11',
-                       nthreads=1,
-                       quad_decimate=1.0,
-                       quad_sigma=0.0,
-                       refine_edges=1,
-                       decode_sharpening=0.25,
-                       debug=0)
+    at_detector = Detector(families='tag36h11',
+                        nthreads=1,
+                        quad_decimate=1.0,
+                        quad_sigma=0.0,
+                        refine_edges=1,
+                        decode_sharpening=0.25,
+                        debug=0)
 
-def array2tuple(p):
-    return (int(p[0]), int(p[1]))
+    tags = at_detector.detect(img)
+    tag_corners = [tag.corners for tag in sorted(tags, key=lambda x: x.tag_id)]
 
-def draw_poly(img, points):
-    points = list(map(array2tuple, points))
-    for i, p in enumerate(points):
-        cv2.line(img, points[i], points[(i + 1) % len(points)], (0, 0, 255), 10)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-tags = at_detector.detect(img)
-tag_corners = [tag.corners for tag in sorted(tags, key=lambda x: x.tag_id)]
+    # we use tag ids 3, 42, 93 and 154 (picked at random) 
+    # order of them in the pdf is upperleft, upperright, lowerleft, lowerright
+    # tag_corners is counterclockwise
+    points = np.float32([
+        tag_corners[0][0],
+        tag_corners[1][1],
+        tag_corners[3][2],
+        tag_corners[2][3],
+    ])
 
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-for c in tag_corners:
-    draw_poly(img, c)
+    new_size = np.float32([
+        [0, 0],
+        [APRILTAG_AREA_WIDTH, 0],
+        [APRILTAG_AREA_WIDTH, APRILTAG_AREA_HEIGHT],
+        [0, APRILTAG_AREA_HEIGHT],
+    ])
+    M = cv2.getPerspectiveTransform(points, new_size)
+    img = cv2.warpPerspective(img, M, (APRILTAG_AREA_WIDTH, APRILTAG_AREA_HEIGHT))
 
-# we use tag ids 3, 42, 93 and 154 (picked at random) 
-# order of them in the pdf is upperleft, upperright, lowerleft, lowerright
-# tag_corners is counterclockwise
-points = np.float32([
-    tag_corners[0][0],
-    tag_corners[1][1],
-    tag_corners[3][2],
-    tag_corners[2][3],
-])
+    img = img[
+        TABLE_OFFSET_X : TABLE_OFFSET_X + TABLE_HEIGHT,
+        TABLE_OFFSET_Y : TABLE_OFFSET_Y + TABLE_WIDTH
+    ]
 
-#draw_poly(img, points)
-new_size = np.float32([
-    [0, 0],
-    [APRILTAG_AREA_WIDTH, 0],
-    [APRILTAG_AREA_WIDTH, APRILTAG_AREA_HEIGHT],
-    [0, APRILTAG_AREA_HEIGHT],
-])
-print(new_size)
-M = cv2.getPerspectiveTransform(points, new_size)
-img = cv2.warpPerspective(img, M, (APRILTAG_AREA_WIDTH, APRILTAG_AREA_HEIGHT))
+    dx = TABLE_WIDTH // 25
+    dy = TABLE_HEIGHT // 13
+    result = []
+    for y in range(0, TABLE_HEIGHT, dy):
+        curr = []
+        for x in range(0, TABLE_WIDTH, dx):
+            x1 = x + dx // 3
+            x2 = x + dx - dx // 3
+            y1 = y + dy // 3
+            y2 = y + dy - dy // 3
 
-img = img[40 : 40 + TABLE_HEIGHT, 680 : 680 + TABLE_WIDTH]
+            if x1 > img.shape[1] or y1 > img.shape[0]:
+                continue
 
-dx = TABLE_WIDTH // 25
-dy = TABLE_HEIGHT // 13
-for y in range(0, TABLE_HEIGHT, dy):
-    for x in range(0, TABLE_WIDTH, dx):
-        x1 = x + dx // 3
-        x2 = x + dx - dx // 3
-        y1 = y + dy // 3
-        y2 = y + dy - dy // 3
+            avg = np.average(img[y1 : y2, x1 : x2])
+            color = (0, 255, 0) if avg > BOX_FILL_THRESH else (0, 0, 255)
+            cv2.circle(img, (x + dx - 30, y + dy - 30), 12, color, -1)
 
-        if x1 > img.shape[1] or y1 > img.shape[0]:
-            continue
+            curr.append(avg <= BOX_FILL_THRESH)
 
-        avg = np.average(img[y1 : y2, x1 : x2])
-        print(x // dx, y // dy, avg)
+        result.append(curr)
 
-        color = (0, 255, 0) if avg > 230 else (0, 0, 255)
-        #cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 5)
-        cv2.circle(img, (x + dx - 30, y + dy - 30), 12, color, -1)
+    return img, result
 
-cv2.imwrite('output.jpg', img)
+if __name__ == "__main__":
+    img = cv2.imread("example-input.jpg")
+    img, ret = analyze_image(img)
+    cv2.imwrite("example-output.jpg", img)
+    print(ret)
