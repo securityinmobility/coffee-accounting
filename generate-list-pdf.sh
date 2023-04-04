abcb9d5bc02298cddc9cbea6c7abf875293d5e3c
Jakob Löw <jakob@m4gnus.de>
2023-04-05 00:12:44 +0200

    :sparkles: add a number below the bottom right apriltag to re-identify lists using the human eye

--- generate-list-pdf.sh
+++ generate-list-pdf.sh
@@ -5,4 +5,7 @@ num="$1"
 wget -O num.png "https://github.com/AprilRobotics/apriltag-imgs/raw/master/tag36h11/tag36_11_001$num.png"
 convert num.png -scale 1600% big_num.png
 
-inkscape apriltagged-list.svg "--export-pdf=apriltagged-list-$num.pdf"
+cp apriltagged-list.svg apriltagged-list.tmp.svg
+sed -i "s/#999#/$1/" apriltagged-list.tmp.svg
+
+inkscape apriltagged-list.tmp.svg "--export-pdf=apriltagged-list-$num.pdf"
